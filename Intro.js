ef3fd1d6d31943213114d0f46cd2298bbfc94b82
Madhusri503 <borramadhusri526@gmail.com>
2025-06-13 09:31:36 +0530

    Add files via upload

---
+++ Intro.js
@@ -0,0 +1,12 @@
+function Intro() {
+        return (
+        <div>
+            <h1>welcome to react js sessions</h1>
+            <p>enjoy the first session</p>
+        </div>
+
+        );
+    }
+    export default Intro;
+
+
